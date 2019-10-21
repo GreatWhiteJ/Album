3614feb3ad7b52f5e51e97b957fa9ae647180753
GreatWhiteJ <48426180+GreatWhiteJ@users.noreply.github.com>
2019-10-21 14:36:14 -0600

    Updated Tokens

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "AIzaSyD22ffuYjuvTPm1u33Y64qbmgmE-fi8m9o",
-  authDomain: "authentication2-a2c4b.firebaseapp.com",
-  databaseURL: "https://authentication2-a2c4b.firebaseio.com",
-  projectId: "authentication2-a2c4b",
-  storageBucket: "",
-  messagingSenderId: "365655359740",
-  appId: "1:365655359740:web:b34c6c42488fcdb0cdd779"
+  apiKey: "AIzaSyASbPDObOysiaBQk2LoTsP1EVoHhxGY7Ks",
+  authDomain: "albumapp-9c7a9.firebaseapp.com",
+  databaseURL: "https://albumapp-9c7a9.firebaseio.com",
+  projectId: "albumapp-9c7a9",
+  storageBucket: "albumapp-9c7a9.appspot.com",
+  messagingSenderId: "241784260295",
+  appId: "1:241784260295:web:37b97abc639a5ed620e0e2"
 };
 
 firebase.initializeApp(config);
